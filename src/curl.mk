a628602353b2c8e3d588d5eeae5deaedd7b31467
Andre Heinecke <aheinecke@intevation.de>
2014-08-27 09:18:33 +0200

    Add polarssl curl with minimal options

--- src/curl.mk
+++ src/curl.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := 0bcd1bd7901ee1d7059189d2b3807fdb6a271bc6
 $(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
 $(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.lzma
 $(PKG)_URL      := http://curl.haxx.se/download/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc gnutls libidn libssh2
+$(PKG)_DEPS     := gcc polarssl
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'http://curl.haxx.se/download/?C=M;O=D' | \
@@ -19,12 +19,37 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --with-gnutls \
+        --without-nghttp2 \
+        --without-libidn \
+        --without-winidn \
+        --without-libssh2 \
+        --without-librtmp \
+        --without-libmetalink \
+        --without-axtls \
+        --without-nss \
+        --without-cyassl \
         --without-ssl \
-        --with-libidn \
-        --enable-sspi \
-        --enable-ipv6 \
-        --with-libssh2
+        --without-gnutls \
+        --disable-gopher \
+        --disable-smtp \
+        --disable-imap \
+        --disable-pop3 \
+        --disable-tftp \
+        --disable-telnet \
+        --disable-dict \
+        --disable-proxy \
+        --disable-rtsp \
+        --disable-ldaps \
+        --disable-ldap \
+        --disable-file \
+        --disable-ftp \
+        --enable-http \
+        --enable-shared=no \
+        --enable-static=yes \
+        --with-polarssl=$(PREFIX)/$(TARGET) \
+        --without-ca-bundle \
+        --without-ca-path
+
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
     ln -sf '$(PREFIX)/$(TARGET)/bin/curl-config' '$(PREFIX)/bin/$(TARGET)-curl-config'
 
